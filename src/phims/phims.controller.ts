8976ff94f896070843138d707e4e0c3253f46ee9
huu77 <thuu28052002@gmail.com>
2023-08-04 15:56:29 +0700

    update , when i send my video to front end , it will update by size but design ...

--- src/phims/phims.controller.ts
+++ src/phims/phims.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Post, Req, Res, UploadedFile, UseInterceptors, Get, UseGuar
 import { PhimsService } from './phims.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
-import path, { extname } from 'path';
+import path, { extname, join } from 'path';
 import { AuthGuard } from 'src/users/auth.guard';
 import { Phim } from './entity/phims.entity';
 import { CreateVideoDto } from './dto/CreateVideo.dto';
@@ -10,7 +10,8 @@ import { multerOptions } from 'src/config/multerConfig';
 import * as express from 'express';
 import { AdminAuthGuard } from 'src/users/adminAuth.guard';
 import { FilterDto } from './dto/Filter.dto';
-
+import * as fs from 'fs';
+import { of } from 'rxjs';
 @UsePipes(new ValidationPipe())
 @Controller('phims')
 export class PhimsController {
@@ -81,11 +82,42 @@ export class PhimsController {
         return this.phimService.getAll(query)
     }
     //GET ONE VIDEO
-    @Get('video/:id')
-    @UseGuards(AuthGuard)
-    serveVideo(@Res() res: Response, @Param('id') id: string) {
-        const videoPath = path.join(__dirname, `../video/${id}`);
-        return express.static(videoPath);
+    @Get(':filename')
+    // @UseGuards(AuthGuard)
+    serveVideo(@Param('filename') filename: string,@Res() res) {
+        const videoPath = join(process.cwd(), 'uploads/'+ filename)
+         
+       if (fs.existsSync(videoPath)) {
+      const stat = fs.statSync(videoPath);
+      const fileSize = stat.size;
+      const range = res.req.headers.range;
+
+      if (range) {
+        const parts = range.replace(/bytes=/, '').split('-');
+        const start = parseInt(parts[0], 10);
+        const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+
+        const chunksize = (end - start) + 1;
+        const file = fs.createReadStream(videoPath, { start, end });
+
+        res.writeHead(206, {
+          'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+          'Accept-Ranges': 'bytes',
+          'Content-Length': chunksize,
+          'Content-Type': 'video/mp4', // Đảm bảo đúng định dạng video
+        });
+
+        file.pipe(res);
+      } else {
+        res.writeHead(200, {
+          'Content-Length': fileSize,
+          'Content-Type': 'video/mp4',
+        });
+        fs.createReadStream(videoPath).pipe(res);
+      }
+    } else {
+      res.status(404).send('Video not found');
+    }
     }
 
     // get phim from to the loai
